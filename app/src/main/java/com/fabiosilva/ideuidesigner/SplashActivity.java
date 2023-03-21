bdedd781554dd37182dbb242e88db4c6aa366dcb
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2023-03-20 23:03:59 -0300

    Add files via upload

--- app/src/main/java/com/fabiosilva/ideuidesigner/SplashActivity.java
+++ app/src/main/java/com/fabiosilva/ideuidesigner/SplashActivity.java
@@ -1,63 +1,55 @@
-package com.fabiosilva.ideuidesigner;
-
+package com.project.incrysy;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+import android.os.Handler;
+import android.content.Context;
 
 public class SplashActivity extends AppCompatActivity {
-    private Timer _timer = new Timer();
-    private TimerTask tempo;
+    private static final int REQUEST_PERMISSIONS_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        start();
-
+        requestPermissions();
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1000) {
-            start();
+        if (requestCode == REQUEST_PERMISSIONS_CODE) {
+            showMainScreen();
         }
     }
 
-    private void start(){
+    private void requestPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
-                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
-                || ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.INTERNET}, 1000);
+			|| ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+			|| ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.INTERNET}, REQUEST_PERMISSIONS_CODE);
+        } else {
+            showMainScreen();
         }
-        else {
-            Thread splash=new Thread() {
-                public void run() {
+    }
 
-                    try{
-
-// set sleep time
-                        sleep(2*1000);
-                        Intent i =new Intent(getBaseContext(),MainActivity.class);
-                        startActivity(i);
-                        finish();
-                    }catch (Exception e){
-
-                    }
-                }
-
-            };
-            splash.start();
-        }
+    private void showMainScreen() {
+		new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					final Context context = SplashActivity.this;
+					Intent i = new Intent(context, StudioActivity.class);
+					startActivity(i);
+					finish();
+				}
+			}, 2000);	
     }
 }
 
