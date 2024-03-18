51fdad8195b60e10ca4669e64e9131dabd9f7c79
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2024-03-17 23:53:43 -0400

    improve

--- app/src/main/java/com/fabiosilva/ideuidesigner/AIDEApplication.java
+++ app/src/main/java/com/fabiosilva/ideuidesigner/AIDEApplication.java
@@ -1,65 +1,82 @@
 package com.project.incrysy;
-import android.app.*;
-import java.io.*;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Application;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-/**import com.google.android.gms.ads.MobileAds;**/
+import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
 
-public class AIDEApplication extends Application
-{
-	private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
-	@Override
-	public void onCreate() {
-		/**MobileAds.initialize(this, "$appid$");**/
-		this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+import com.google.android.gms.ads.MobileAds;
 
-		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-			@Override
-			public void uncaughtException(Thread thread, Throwable ex) {
-				Intent intent = new Intent(getApplicationContext(), DebugActivity.class);
-				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-				intent.putExtra("error", getStackTrace(ex));
+public class AIDEApplication extends Application {
+    private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+    private ExecutorService executorService;
 
-				PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 11111, intent, PendingIntent.FLAG_ONE_SHOT);
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
+        MobileAds.initialize(this, "$appid$");
 
-				AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-				am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, pendingIntent);
+        this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
 
-				android.os.Process.killProcess(android.os.Process.myPid());
-				System.exit(2);
+        this.executorService = Executors.newSingleThreadExecutor();
 
-				uncaughtExceptionHandler.uncaughtException(thread, ex);
-			}
-		});
-		super.onCreate();
+        executorService.execute(() -> {
+            Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+                @Override
+                public void uncaughtException(Thread thread, Throwable ex) {
+                    Intent intent = new Intent(getApplicationContext(), DebugActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
-	}
+                    Writer result = new StringWriter();
+                    PrintWriter printWriter = new PrintWriter(result);
+                    ex.printStackTrace(printWriter);
+                    String stackTrace = result.toString();
 
+                    intent.putExtra("error", stackTrace);
 
-	private String getStackTrace(Throwable th){
-		Exception e = new Exception(th);
-		StringWriter result = new StringWriter();
-		PrintWriter printWriter = new PrintWriter(result);
-		while(th != null){
-			th.printStackTrace(printWriter);
-			th = th.getCause();
-		}
-		String r = result.toString();
-		
-		//Uncomment below lines to write logs to local storage when your app crashes
-		//Make sure you request storage permissions on devices with API 23+
-		
-		
-		//result = new StringWriter();
-		//printWriter = new PrintWriter(result);
-		//e.printStackTrace(printWriter);
-		//String r2 = result.toString();
-		//FileUtil.writeFile(FileUtil.getExternalStorageDir() + "/logcat.txt", r2.toString());
-		
-		return r;
-	}
-}
+                    PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 11111, intent, PendingIntent.FLAG_ONE_SHOT);
+
+                    AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, pendingIntent);
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                        try {
+                            uncaughtExceptionHandler.uncaughtException(thread, ex);
+                        } catch (Throwable throwable) {
+                            Log.e("AIDEApplication", "Error in uncaughtExceptionHandler", throwable);
+                        }
+                    } else {
+                        uncaughtExceptionHandler.uncaughtException(thread, ex);
+                    }
+
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(2);
+                }
+            });
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void showCrashDialog(final String error) {
+        executorService.execute(() -> {
+            runOnUiThread(() -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Cr
