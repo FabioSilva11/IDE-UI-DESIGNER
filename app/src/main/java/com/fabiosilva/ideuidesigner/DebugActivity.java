913916a12712b014774972817edf1cb88b2b8e72
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2024-03-17 23:53:39 -0400

    improve

--- app/src/main/java/com/fabiosilva/ideuidesigner/DebugActivity.java
+++ app/src/main/java/com/fabiosilva/ideuidesigner/DebugActivity.java
@@ -1,71 +1,65 @@
 package com.project.incrysy;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.io.InputStream;
 
-
 public class DebugActivity extends Activity {
-
-	String[] exceptionType = {
-			"",
-			"",
-			"",
-			"",
-			""
-
-	};
-
-	String[] errMessage= new String[]{"","","","",""};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Intent intent = getIntent();
+        String errMsg = "";
+        String madeErrMsg = "";
 
+        if (intent != null) {
+            errMsg = intent.getStringExtra("error");
 
-		Intent intent = getIntent();
-		String errMsg = "";
-		String madeErrMsg = "";
-		if(intent != null){
-			errMsg = intent.getStringExtra("error");
+            if (errMsg != null && !errMsg.isEmpty()) {
+                String[] spilt = errMsg.split("\n");
 
-			String[] spilt = errMsg.split("\n");
-			//errMsg = spilt[0];
-			try {
-				for (int j = 0; j < exceptionType.length; j++) {
-					if (spilt[0].contains(exceptionType[j])) {
-						madeErrMsg = errMessage[j];
+                try {
+                    for (String line : spilt) {
+                        if (!line.isEmpty()) {
+                            madeErrMsg = parseErrorMessage(line);
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    Log.e("DebugActivity", "Error parsing error message", e);
+                }
 
-						int addIndex = spilt[0].indexOf(exceptionType[j]) + exceptionType[j].length();
+                if (madeErrMsg.isEmpty()) {
+                    madeErrMsg = errMsg;
+                }
+            }
+        }
 
-						madeErrMsg += spilt[0].substring(addIndex, spilt[0].length());
-						break;
-
-					}
-				}
-
-				if(madeErrMsg.isEmpty()) madeErrMsg = errMsg;
-			}catch(Exception e){}
-
-		}
-
-        AlertDialog.Builder bld = new AlertDialog.Builder(this);
-
-		bld.setTitle("An error occured");
-		bld.setMessage( madeErrMsg );
-		bld.setNeutralButton("End Application", new DialogInterface.OnClickListener() {
-
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				finish();
-			}
-		});
-		bld.create().show();
-
+        showDialog(madeErrMsg);
     }
-}
+
+    private String parseErrorMessage(String line) {
+        String message = "";
+        String[] exceptionTypes = {"Exception", "Error", "AssertionError", "RuntimeException"};
+
+        for (String exceptionType : exceptionTypes) {
+            if (line.contains(exceptionType)) {
+                int index = line.indexOf(exceptionType) + exceptionType.length();
+                message = line.substring(index);
+                break;
+            }
+        }
+
+        return message;
+    }
+
+    private void showDialog(String message) {
+        if (message != null && !message.isEmpty()) {
+            AlertDialog.Builder bld = new Al
