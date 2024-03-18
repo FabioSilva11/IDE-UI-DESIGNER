260e6ad5e7182ee741ccb5afb968cc1d99c5597e
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2024-03-17 23:53:38 -0400

    improve

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,3 @@
-@rem
 @rem Copyright 2015 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,9 +11,9 @@
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
-@rem
 
 @if "%DEBUG%" == "" @echo off
+
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
@@ -24,66 +23,51 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+set "DIRNAME=%~dp0"
+if "%DIRNAME%" == "" set "DIRNAME=."
+set "APP_BASE_NAME=%~n0"
+set "APP_HOME=%DIRNAME%"
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+for %%I in ("%APP_HOME%") do set "APP_HOME=%%~fI"
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set "DEFAULT_JVM_OPTS=-Xmx64m -Xms64m"
 
 @rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
+if defined JAVA_HOME (
+    set "JAVA_HOME=%JAVA_HOME:"=%"
+    set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
+) else (
+    set "JAVA_EXE=java.exe"
+)
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto execute
+if exist "%JAVA_EXE%" (
+    %JAVA_EXE% -version >NUL 2>&1
+) else (
+    echo.
+    echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+    echo.
+    echo Please set the JAVA_HOME variable in your environment to match the
+    echo location of your Java installation.
+    goto :fail
+)
 
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+if "%ERRORLEVEL%" == "0" (
+    @rem Setup the command line
+    set "CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar"
 
-goto fail
-
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-
-if exist "%JAVA_EXE%" goto execute
-
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-
-goto fail
-
-:execute
-@rem Setup the command line
-
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+    @rem Execute Gradle
+    "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+) else (
+    echo.
+    echo ERROR: Java version is not compatible.
+    echo.
+    goto :fail
+)
 
 :end
 @rem End local scope for the variables with windows NT shell
-if "%ERRORLEVEL%"=="0" goto mainEnd
-
-:fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
-exit /b 1
-
-:mainEnd
 if "%OS%"=="Windows_NT" endlocal
 
 :omega
