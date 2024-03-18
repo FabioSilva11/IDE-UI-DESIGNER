111299a198d98cf631488562ba6b0072633a1b27
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2024-03-17 23:53:32 -0400

    improve

--- app/src/main/java/com/fabiosilva/ideuidesigner/Projeto.java
+++ app/src/main/java/com/fabiosilva/ideuidesigner/Projeto.java
@@ -1,15 +1,20 @@
 package com.fabiosilva.ideuidesigner;
 
-public class Projeto {
-    String version = "", name = "", release = "", data = "";
+public class Project {
+    private String version;
+    private String name;
+    private String releaseDate;
+    private String creationDate;
 
-    public Projeto(String version, String name, String release, String data) {
+    // Constructor that initializes all fields
+    public Project(String version, String name, String releaseDate, String creationDate) {
         this.version = version;
         this.name = name;
-        this.release = release;
-        this.data = data;
+        this.releaseDate = releaseDate;
+        this.creationDate = creationDate;
     }
 
+    // Getter methods for all fields
     public String getVersion() {
         return version;
     }
@@ -18,11 +23,11 @@ public class Projeto {
         return name;
     }
 
-    public String getRelease() {
-        return release;
+    public String getReleaseDate() {
+        return releaseDate;
     }
 
-    public String getData() {
-        return data;
+    public String getCreationDate() {
+        return creationDate;
     }
 }
